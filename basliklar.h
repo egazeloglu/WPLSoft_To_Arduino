8f8f1b648d0979a855816e5913f9662f57864471
egazeloglu <egazeloglu@gmail.com>
2026-01-15 12:49:55 +0300

    Arduino Ladder Programlama
    Delta WPLSoft IDE kullanarak Arduino programlama Arayüz yazılımıdır.

---
+++ basliklar.h
@@ -0,0 +1,16 @@
+#define X0 2
+#define X1 3
+#define X2 4
+#define X3 5
+#define X4 6
+#define X5 7
+#define X6 8
+#define X7 9
+#define Y0 10
+#define Y1 11
+#define Y2 12
+#define Y3 13
+#define Y4 14
+#define AI0 15
+#define AI1 16
+
